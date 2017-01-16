7bfd85c96c41678c49e85df94c9433804159cdc9
Hanita <atounh@gmail.com>
2017-01-16 14:55:47 +0200

    how old are you message

---
+++ aMessageToYou.js
@@ -0,0 +1,35 @@
+// JavaScript source code
+var a=16
+var b=23
+var c=42
+var d=53
+var e=100
+
+var age = 16;
+
+switch (age)
+{
+    case age == a:
+        alert("Have a ball your life is starting!!");
+        break;
+
+    case age == b:
+        alert("good time to learn!!");
+        break;
+
+    case age == c:
+        alert("dont worry its only a game!!");
+        break;
+
+    case age == d:
+        alert("Tell us some interesting stories!!");
+        break;
+
+    case age == e:
+        alert("Wow you must really like it here!!");
+        break;
+
+    default:
+        alert("never mind...");
+
+}
